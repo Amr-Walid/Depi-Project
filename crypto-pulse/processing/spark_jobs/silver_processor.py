17b84735c28ce3023c6be73aaeac84e4e61c1d2e
amrwalid <10212022100768@stud.cu.edu.eg>
2026-04-25 23:30:04 +0300

    fix(silver): change partitioning to year to fix small file issue

--- crypto-pulse/processing/spark_jobs/silver_processor.py
+++ crypto-pulse/processing/spark_jobs/silver_processor.py
@@ -131,7 +131,7 @@ def process_bronze_to_silver(spark, config):
             processed_df.write
             .format("delta")
             .mode("overwrite")
-            .partitionBy("year", "month", "day")
+            .partitionBy("year")
             .save(silver_path)
         )
 
