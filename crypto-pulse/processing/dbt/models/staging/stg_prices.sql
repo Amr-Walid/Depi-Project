034b38c986213ebe04a57aeefe610a95b26821a7
Kareem Ahmed <48216407+kareem56@users.noreply.github.com>
2026-04-29 18:55:43 +0300

    Refactor stg_prices.sql to select from silver_prices

--- crypto-pulse/processing/dbt/models/staging/stg_prices.sql
+++ crypto-pulse/processing/dbt/models/staging/stg_prices.sql
@@ -2,10 +2,11 @@
 
 SELECT
     symbol,
-    CAST(price AS DECIMAL(18, 8)) AS price,
-    CAST(volume AS DECIMAL(18, 8)) AS volume,
-    CAST(timestamp AS TIMESTAMP) AS event_time,
-    ingested_at
-FROM {{ source('silver', 'bronze_historical_prices') }}
+    price,
+    volume_24h AS volume,
+    event_time,
+    source,
+    processed_at
+FROM {{ source('silver_prices', 'prices') }}
 WHERE price IS NOT NULL
   AND price > 0
